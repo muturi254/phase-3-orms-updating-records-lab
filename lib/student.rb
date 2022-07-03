3a76e1d2f67a1d9a812e871ae25d5213f6ec304f
muturi25 <petermuturi74@gmail.com>
2022-07-03 14:58:45 +0300

    filter or create method

--- lib/student.rb
+++ lib/student.rb
@@ -47,7 +47,7 @@ class Student
 
   end
 
-  def self.create(name, grade)
+  def self.create(name:, grade:)
     student = Student.new(name, grade)
     student.save
   end
@@ -73,4 +73,15 @@ class Student
 
     DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
+
+
+  def self.find_or_create_by(name:, grade:)
+    student = DB[:conn].execute("SELECT * FROM students WHERE name=? AND breed=?", name, breed)
+    if (!student.empty?)
+      student_data = student[0]
+      student =  Student.new(student_data[0], student_data[1], student_data[2])
+    else
+      student = Student.create(name: name, grade: grade)
+    end
+    student
 end
